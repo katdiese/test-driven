4d45f3da058ff49cb24a8784ef25345953c0a324
Kathy Dieser <katdiese@gmail.com>
2016-05-09 14:20:20 -0600

    added prelim route to query db

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var queries = require('../../../queries');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  queries.getDecks()
+  .then(function(data) {
+    res.status(200)
+    .json({
+      data: data,
+      message: 'this worked!'
+    })
+  })
 });
 
 module.exports = router;
