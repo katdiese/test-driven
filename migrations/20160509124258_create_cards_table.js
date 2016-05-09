ed0f6dd12a25048f285fb49d847b96773399cbf4
Kathy Dieser <katdiese@gmail.com>
2016-05-09 16:10:16 -0600

    added image url to data

--- migrations/20160509124258_create_cards_table.js
+++ migrations/20160509124258_create_cards_table.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('cards', function(table) {
     table.increments('card_id');
     table.integer('deck_id').references('deck_id').inTable('decks');
+    table.text('card_url').defaultTo('https://image.freepik.com/free-icon/question-mark_318-52837.jpg');
     table.text('question');
     table.text('answer');
   })
